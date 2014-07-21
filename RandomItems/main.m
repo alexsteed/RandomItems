aa9e32362201833d11205cde8862290bf42e0ded
Alexis Bastide <alexis@Alexiss-MacBook-Pro.local>
2014-07-21 13:47:46 +0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Alexis Bastide on 21/07/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
